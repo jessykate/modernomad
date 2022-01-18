2a389f5340926ab36d7d54630a491a2850692be7
MarcManiez <marc.maniez@gmail.com>
2022-01-18 15:28:32 -0800

    Allow ws connection cross origin I think this fixes it?

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -47,4 +47,13 @@ module.exports = {
       url: false,
     },
   },
+
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+  },
 };
