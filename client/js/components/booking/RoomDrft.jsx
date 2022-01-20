c7571cc2d4414d1de737f059f2eb8a2b79086133
MarcManiez <marc.maniez@gmail.com>
2022-01-19 16:41:30 -0800

    Convert to function component

--- client/js/components/booking/RoomDrft.jsx
+++ client/js/components/booking/RoomDrft.jsx
@@ -1,132 +1,118 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-// import { browserHistory } from 'react-router'
-import RoomDrftIndex from './RoomDrftIndex'
-import RoomDetail from './RoomDetail'
-import gql from 'graphql-tag'
-import { graphql } from 'react-apollo'
-import _ from 'lodash'
-import moment from 'moment'
-import Loader from '../generic/Loader'
+import React, { useEffect } from "react";
+import RoomDrftIndex from "./RoomDrftIndex";
+import RoomDetail from "./RoomDetail";
+import gql from "graphql-tag";
+import qs from "qs";
+import _ from "lodash";
+import moment from "moment";
+import Loader from "../generic/Loader";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
-const resourcesQuery = gql`
-query AllResourcesForLocation($arrive: DateTime!, $depart: DateTime!) {
-  allLocations {
-    edges {
-      node {
-        name
-        slug
-        resources(hasFutureDrftCapacity: true){
-          id
-          rid
+const GET_RESOURCES = gql`
+  query AllResourcesForLocation($arrive: DateTime!, $depart: DateTime!) {
+    allLocations {
+      edges {
+        node {
           name
-          image
-          defaultRate
-          hasFutureDrftCapacity
-          acceptDrftTheseDates(arrive: $arrive, depart: $depart)
-          availabilities(arrive: $arrive, depart: $depart) {
-            date
-            quantity
+          slug
+          resources(hasFutureDrftCapacity: true) {
+            id
+            rid
+            name
+            image
+            defaultRate
+            hasFutureDrftCapacity
+            acceptDrftTheseDates(arrive: $arrive, depart: $depart)
+            availabilities(arrive: $arrive, depart: $depart) {
+              date
+              quantity
+            }
           }
         }
       }
     }
   }
-}
 `;
 
-class RoomDrftIndexOrDetailWithoutQuery extends React.Component {
-  constructor(props) {
-    super()
-  }
+function RoomDrftIndexOrDetail({ children }) {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const search = useLocation().search.slice(1);
+  const query = qs.parse(search);
+  const arrive = query.arrive
+    ? moment(query.arrive, parseFormat)
+    : moment().startOf("day");
+  const depart = query.depart
+    ? moment(query.depart, parseFormat)
+    : arrive.clone().add(7, "days");
+  const { loading, error, data } = useQuery(GET_RESOURCES, {
+    variables: { arrive, depart },
+  });
 
-  renderSubComponent() {
-    const routeParams = this.props.routeParams
-    const child = this.props.children
-    const sharedProps = {
-      ...this.props,
-      onFilterChange: this.reFilter.bind(this),
-      query: this.props.location.query,
-      loading: this.props.data.loading
+  const locationData = () => {
+    const queryResults = data.allLocations;
+    return queryResults && queryResults.edges.length > 0
+      ? queryResults.edges
+      : null;
+  };
+
+  const allResources = () => {
+    const location = locationData();
+    return location ? location : [];
+  };
+
+  const oneResource = (id) => {
+    return _.find(allResources(), { rid: parseInt(id) });
+  };
+
+  const reFilter = (filters) => {
+    const formattedDates = {
+      arrive: moment(filters.dates.arrive).format("MM/DD/YYYY"),
+      depart: moment(filters.dates.depart).format("MM/DD/YYYY"),
+    };
+    let path = "/drft/";
+
+    if (id) {
+      path = path + "room/" + id;
     }
 
-    if (!!child) {
-      const roomID = child.props.routeParams.id
-      const room = this.oneResource(roomID)
+    navigate({
+      pathname: path,
+      query: `?${formattedDates}`,
+    });
+  };
+
+  const renderSubComponent = () => {
+    const sharedProps = {
+      onFilterChange: reFilter,
+      query,
+      loading,
+    };
+
+    if (!!children) {
+      console.log(loading, data);
+      debugger;
+      const roomID = children.props.routeParams.id;
+      const room = oneResource(roomID);
 
       if (room) {
         return (
           <RoomDetail {...sharedProps} room={room}>
-            {this.props.children}
+            {children}
           </RoomDetail>
-        )
+        );
       } else {
         return null;
       }
     } else {
-      return <RoomDrftIndex {...sharedProps} rooms={this.allResources()} />
+      return <RoomDrftIndex {...sharedProps} rooms={allResources()} />;
     }
-  }
+  };
 
-  reFilter(filters) {
-    const formattedDates = {arrive: filters.dates.arrive.format('MM/DD/YYYY'), depart: filters.dates.depart.format('MM/DD/YYYY')}
-    var path = '/drft/'
-
-    if (this.props.params.id) {
-      path = path + 'room/' + this.props.params.id
-    }
-
-    browserHistory.push({
-      pathname: path,
-      query: formattedDates
-    })
-  }
-
-  locationData() {
-    const queryResults = this.props.data.allLocations
-    if (queryResults && queryResults.edges.length > 0) {
-      return queryResults.edges
-    } else {
-      return null
-    }
-  }
-
-  allResources() {
-    const location = this.locationData()
-    if (location) {
-      return location
-    } else {
-      return []
-    }
-  }
-
-  oneResource(id) {
-    return _.find(this.allResources(), {rid: parseInt(id)})
-  }
-
-  render() {
-    return (
-      <Loader loading={this.props.data.loading}>
-        {this.renderSubComponent()}
-      </Loader>
-    )
-  }
+  return (
+    <Loader loading={loading}>{loading ? null : renderSubComponent()}</Loader>
+  );
 }
 
-const RoomDrftIndexOrDetail = graphql(resourcesQuery, {
-  options: (props) => {
-    const formatString = 'Y-MM-DTHH:mm:ss.ms'
-    const parseFormat = 'MM/DD/YYYY'
-    const query = props.location.query
-    const arrive = query.arrive ? moment(query.arrive, parseFormat) : moment().startOf('day')
-    const depart = query.depart ? moment(query.depart, parseFormat) : arrive.clone().add(7, 'days')
-
-    return {
-      variables: {
-        arrive: arrive.format(formatString),
-        depart: depart.format(formatString)
-      }
-    }
-  }
-})(RoomDrftIndexOrDetailWithoutQuery)
-export default RoomDrftIndexOrDetail
+export default RoomDrftIndexOrDetail;
