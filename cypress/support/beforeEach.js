c44112e40a45df5010e0f00778e221aa900d7bb0
MarcManiez <marc.maniez@gmail.com>
2022-02-01 18:41:28 -0800

    Add instructions to run cypress outside docker

--- cypress/support/beforeEach.js
+++ cypress/support/beforeEach.js
@@ -1,7 +1,17 @@
-beforeEach(function() {
+beforeEach(function () {
   // Reset DB before each test
   // TODO: find a faster way to do this.
   cy.exec(
     "docker-compose run web sh -c './manage.py flush --noinput && ./manage.py generate_test_data'"
   );
+
+  /**
+   * This command performs the same function as the docker-based command above,
+   * except when running tests in a local, non-containerized environment.
+   *
+   * It specifies a test database that should be created beforehand
+   */
+  // cy.exec(
+  //   `source .venv/bin/activate && DATABASE_URL="postgres:///modernomad/test" python manage.py flush --no-input && DATABASE_URL="postgres:///modernomad/test" python manage.py generate_test_data`
+  // );
 });
